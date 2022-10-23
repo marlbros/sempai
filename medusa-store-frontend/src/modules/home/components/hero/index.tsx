ccf5b3bafedcbd70ecdf6be34a7211e96b6f5c5b
Rupsnigdha <83987752+Rupsnigdha@users.noreply.github.com>
2022-10-24 02:23:23 +0530

    home page

--- medusa-store-frontend/src/modules/home/components/hero/index.tsx
+++ medusa-store-frontend/src/modules/home/components/hero/index.tsx
@@ -4,22 +4,21 @@ import Image from "next/image"
 const Hero = () => {
   return (
     <div className="h-[90vh] w-full relative">
-      <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
-        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          Summer styles are finally here
+      <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32 text-gray-800">
+        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black text-gray-900">
+          T- Shirts are here!
         </h1>
-        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-          This year, our new summer collection will shelter you from the harsh
-          elements of a world that doesn&apos;t care if you live or die.
+        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black text-gray-900">
+          Get your very own cool anime T-Shirt to show it off to your friends.
         </p>
-        <UnderlineLink href="/store">Explore products</UnderlineLink>
+        <UnderlineLink href="/store">Explore T-Shirts</UnderlineLink>
       </div>
       <Image
-        src="/hero.jpg"
+        src="/hero1.png"
         layout="fill"
         loading="eager"
         priority={true}
-        quality={90}
+        quality={100}
         objectFit="cover"
         alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
         className="absolute inset-0"
